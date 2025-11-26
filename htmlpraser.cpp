1b0b70a7499aa034fac02d187d488555a2ec5d85
mahasunil <sun5maha@gmail.com>
2025-11-26 10:03:13 +0000

    added the attributes handling feature

--- htmlpraser.cpp
+++ htmlpraser.cpp
@@ -1,96 +1,195 @@
-#include<iostream>
-#include<vector>
-#include<stack>
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <sstream>
+#include <map>
 
 using namespace std;
 
-class Node{
-    private:
+class Node
+{
+private:
     string tagType;
     string textContent;
-    vector<Node*>children;
+    map<string, string, greater<string>> attributes;
+    vector<Node *> children;
 
-    public:
-    ~Node(){
-        for(auto child:children){
+public:
+    ~Node()
+    {
+        for (auto child : children)
+        {
             delete child;
         }
     }
 
-    void addTextContent(const string &text){
+    void addTextContent(const string &text)
+    {
         textContent += text;
     }
-    void addTagType(const string &tag){
-        tagType=tag;
+    void addTagType(const string &tag)
+    {
+        tagType = tag;
     }
-    void addChildren(Node *node){
+    void addAttribute(const string &key, const string &val)
+    {
+        attributes[key] = val;
+    }
+    void addChildren(Node *node)
+    {
         children.push_back(node);
     }
-    void print(int depth=0){
-        for(int i=0; i<depth; i++) cout<<" ";
-        cout<<tagType;
-        if(!textContent.empty()) cout<<": "<<textContent;
-        cout<<endl;
 
-        for(auto &child:children){
-            child->print(depth+1);
+    void print(int depth = 0)
+    {
+        for (int i = 0; i < depth; i++)
+            cout << " ";
+        cout << tagType;
+
+        if (!attributes.empty())
+        {
+            cout << ":";
+            cout << "[";
+            bool first = true;
+            for (auto &attr : attributes)
+            {
+                if (!first)
+                    cout << ", ";
+                cout << attr.first << "=\"" << attr.second << "\"";
+                first = false;
+            }
+            cout << "]";
+        }
+
+        if (!textContent.empty())
+            cout << ": " << textContent;
+        cout << endl;
+
+        for (auto &child : children)
+        {
+            child->print(depth + 1);
         }
     }
 };
 
-class ParseHTML{
-    private:
+class ParseHTML
+{
+private:
     string tagName;
     string texts;
-    Node *root=nullptr;
-    bool insideTag=false;
-    stack<Node*>st;
+    Node *root = nullptr;
+    bool insideTag = false;
+    stack<Node *> st;
 
-    public:
-    ~ParseHTML(){
+public:
+    ~ParseHTML()
+    {
         delete root;
     }
 
-    ParseHTML(const string &html){
-        for(size_t i=0; i<html.size(); i++){
-            char ch=html[i];
-            if(ch=='<'){
-                if(!texts.empty() && !st.empty()){
+    ParseHTML(const string &html)
+    {
+        for (size_t i = 0; i < html.size(); i++)
+        {
+            char ch = html[i];
+            if (ch == '<')
+            {
+                if (!texts.empty() && !st.empty())
+                {
                     st.top()->addTextContent(texts);
                 }
-                insideTag=true;
-                tagName="";
-                texts="";
-            }else if(ch=='>'){
-                insideTag=false;
-                if(tagName[0]=='/'){
-                    if(!st.empty()){
+                insideTag = true;
+                tagName = "";
+                texts = "";
+            }
+            else if (ch == '>')
+            {
+                insideTag = false;
+                if (tagName[0] == '/')
+                {
+                    if (!st.empty())
+                    {
                         st.pop();
                     }
-                }else{
-                    Node *newNode=new Node();
-                    newNode->addTagType(tagName);
-                    if(st.empty()){
-                        root=newNode;
-                    }else{
+                }
+                else
+                {
+                    stringstream ss(tagName);
+                    string tagText;
+                    ss >> tagText;
+
+                    Node *newNode = new Node();
+                    newNode->addTagType(tagText);
+
+                    while (ss >> ws && ss.peek() != EOF)
+                    {
+                        string key, value;
+                        while (ss.peek() != EOF && ss.peek() != '=' && !isspace(ss.peek()))
+                        {
+                            key.push_back(ss.get());
+                        }
+                        ss >> ws;
+                        if (ss.peek() == '=')
+                        {
+                            ss.get();
+                            ss >> ws;
+                            if (ss.peek() == '"' || ss.peek() == '\'')
+                            {
+                                char quote = ss.get();
+
+                                while (ss.peek() != quote && ss.peek() != EOF)
+                                {
+                                    value.push_back(ss.get());
+                                }
+                                if (ss.peek() == quote)
+                                {
+                                    ss.get();
+                                }
+                            }
+                            else
+                            {
+                                while (ss.peek() != EOF && !isspace(ss.peek()))
+                                {
+                                    value.push_back(ss.get());
+                                }
+                            }
+                        }
+                        if (!key.empty())
+                        {
+                            newNode->addAttribute(key, value);
+                        }
+                    }
+
+                    if (st.empty())
+                    {
+                        root = newNode;
+                    }
+                    else
+                    {
                         st.top()->addChildren(newNode);
                     }
                     st.push(newNode);
                 }
-            }else{
-                if(insideTag) tagName += ch;
-                else texts += ch;
+            }
+            else
+            {
+                if (insideTag)
+                    tagName += ch;
+                else
+                    texts += ch;
             }
         }
     }
 
-    Node* getRoot() const {
+    Node *getRoot() const
+    {
         return root;
     }
 };
 
-int main(){
-    string html="<html><body><h1>This is heading.</h1><p>This is paragraph.</p></body></html>";
+int main()
+{
+    string html = R"(<html><body><h1 id="mainHeader" class="title">This is heading.</h1><p class="para">This is paragraph.</p></body></html>)";
     ParseHTML parse(html);
     Node *rootNode = parse.getRoot();
     rootNode->print();
